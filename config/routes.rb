8b7d5602e56a0d47f56b2ef4b9a0b049c77fac22
bryangoonzalez <bryangoonzalez@gmail.com>
2020-04-13 23:32:39 -0400

    Live chat feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  #post/get/delete are the functions of routes. One sends info, one gets and one destroys.
+  #the to: function directs it to a controller method. In this case, messages controller
+  #and the create method --> def create section.
+  post 'message', to: 'messages#create'
+  mount ActionCable.server, at: '/cable'
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Erika", password: "password")
+
+
+Message.create(body: "Hello world", user: User.last)
